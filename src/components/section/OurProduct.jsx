6bc5d727497abc958a7fd26eab4369e612cd951a
esakenyun <rfahlevih@gmail.com>
2024-11-29 19:05:04 +0700

    change padding y in our product

--- src/components/section/OurProduct.jsx
+++ src/components/section/OurProduct.jsx
@@ -60,7 +60,7 @@ export default function OurProductSection() {
   ];
 
   return (
-    <div className="px-[5%] md:px-[3%] pt-[7%] py-[3%] md:py-0">
+    <div className="px-[5%] md:px-[3%] py-[7%]">
       <p className="text-right text-2xl text-primary-600 font-medium">Our Product</p>
       <div className="flex justify-center w-full pt-[3%]">
         <div className="w-full rounded-lg backdrop-blur-2xl">
